9f06d60643642b095988052b93b75c0c809f6d88
Bryan Mark Fajutag <fbryanmark@gmail.com>
2019-10-19 14:45:51 +0800

    Added key values for linkedin

--- config/key.js
+++ config/key.js
@@ -1,28 +1,32 @@
 module.exports = {
-    google: {
-        clientID: '',
-        clientSecret: ''
-    },
-    mongodb: {
-        dbURI: 'YOUR_ID'
-    },
-    session: {
-        cookieKey: 'secret_key'
-    },
-    facebook:{
-        clientID:'',
-        clientSecret:'',
-    },
-    twitter:{
-        clientID: '',
-        clientSecret: ''
-    },
-    reddit:{
-        clientID: '',
-        clientSecret:'',
-    },
-    github:{
-        clientID: '',
-        clientSecret: '',
-    }
+  google: {
+    clientID: '',
+    clientSecret: ''
+  },
+  mongodb: {
+    dbURI: 'YOUR_ID'
+  },
+  session: {
+    cookieKey: 'secret_key'
+  },
+  facebook: {
+    clientID:'',
+    clientSecret:'',
+  },
+  twitter: {
+    clientID: '',
+    clientSecret: ''
+  },
+  reddit: {
+    clientID: '',
+    clientSecret:'',
+  },
+  github: {
+    clientID: '',
+    clientSecret: '',
+  },
+  linkedin: {
+    clientID: '',
+    clientSecret: '',
+  },
 };
