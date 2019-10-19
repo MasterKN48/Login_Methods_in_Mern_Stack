98749a5ff154f84cb0d024abb76bb86f35e748d1
Bryan Mark Fajutag <fbryanmark@gmail.com>
2019-10-19 14:43:08 +0800

    Fixed indentation; Added endpoints for logging in with linkedin

--- routes/auth.js
+++ routes/auth.js
@@ -1,102 +1,141 @@
-const User=require('../models/user');
-const router=require('express').Router();
+const User = require('../models/user');
+const router = require('express').Router();
 const passport = require('passport');
 
-router.post('/register',(req, res)=>{
-	var password = req.body.password;
-	var password2 = req.body.password2;
+router.post('/register', (req, res) => {
+  var password = req.body.password;
+  var password2 = req.body.password2;
 
-  if (password == password2){
+  if (password == password2) {
     var newUser = new User({
-  		name: req.body.name,
-  		email: req.body.email,
-  		username: req.body.username,
-  		password: req.body.password
-  	});
+      name: req.body.name,
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password
+    });
 
-  	User.createUser(newUser, function(err, user){
-  		if(err) throw err;
-  		res.send(user).end()
-  	});
-  } else{
+    User.createUser(newUser, function (err, user) {
+      if (err) throw err;
+      res.send(user).end()
+    });
+  } else {
     res.status(500).send("{errors: \"Passwords don't match\"}").end()
   }
 });
 
 // Endpoint to login
 router.post('/login',
-  passport.authenticate('local',{failureRedirect: '/sorry'}),
-  (req, res) =>{
-    res.status(200).json({msg:'login success',user:req.user});
+  passport.authenticate('local', {
+    failureRedirect: '/sorry'
+  }),
+  (req, res) => {
+    res.status(200).json({
+      msg: 'login success',
+      user: req.user
+    });
   }
 );
 
 // Endpoint to get current user
-router.get('/user',passport.authenticate('local',{failureRedirect: '/login'}),(req, res)=>{
-	res.status(200).json({msg:'current User',user:req.user});
+router.get('/user', passport.authenticate('local', {
+  failureRedirect: '/login'
+}), (req, res) => {
+  res.status(200).json({
+    msg: 'current User',
+    user: req.user
+  });
 })
 
-
 // Endpoint to logout
-router.get('/logout',(req, res)=>{
-	req.logout();
-	res.json({msg:'logout'})
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.json({
+    msg: 'logout'
+  })
 });
 
 // login with google
 router.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile','email'] }));
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  }));
 
-router.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
+router.get('/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/login'
+  }),
+  function (req, res) {
     // Successful authentication, redirect home.
-    res.json({user:req.user})
+    res.json({
+      user: req.user
+    })
   });
 
-
 // login with facebook
-router.get('/auth/facebook',passport.authenticate('facebook')); //eg { scope: ['user_friends', 'manage_pages'] }
-
+router.get('/auth/facebook', passport.authenticate('facebook')); //eg { scope: ['user_friends', 'manage_pages'] }
 router.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/login' }),
-  function(req, res) {
+  passport.authenticate('facebook', {
+    failureRedirect: '/login'
+  }),
+  function (req, res) {
     // Successful authentication, redirect home.
-    res.status(200).json({msg:'success',user:req.user});
+    res.status(200).json({
+      msg: 'success',
+      user: req.user
+    });
   }
 );
 
 //login with twitter
 router.get('/auth/twitter', passport.authenticate('twitter'));
-
 router.get('/auth/twitter/callback',
-  passport.authenticate('twitter', { failureRedirect: '/login'}),
-  function(req, res){
+  passport.authenticate('twitter', {
+    failureRedirect: '/login'
+  }),
+  function (req, res) {
     // Successful authentication, redirect home.
-    res.status(200).json({msg:'success',user:req.user});
+    res.status(200).json({
+      msg: 'success',
+      user: req.user
+    });
   }
 );
 
 //login with reddit
 router.get('/auth/reddit', passport.authenticate('reddit'));
-	
-
-	router.get('/auth/reddit/callback',
-	  passport.authenticate('reddit', { failureRedirect: '/login'}),
-	  function(req, res){
-	    // Successful authentication, redirect home.
-	    res.status(200).json({msg:'success',user:req.user});
-	  }
-	);
-
-router.get('/auth/github', passport.authenticate('github'));
-
-router.get('/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login'}),
-  function(req, res){
+router.get('/auth/reddit/callback',
+  passport.authenticate('reddit', {
+    failureRedirect: '/login'
+  }),
+  function (req, res) {
     // Successful authentication, redirect home.
-    res.status(200).json({msg:'success',user:req.user});
+    res.status(200).json({
+      msg: 'success',
+      user: req.user
+    });
   }
 );
 
-module.exports=router;
+//login with github
+router.get('/auth/github', passport.authenticate('github'));
+router.get('/auth/github/callback',
+  passport.authenticate('github', {
+    failureRedirect: '/login'
+  }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.status(200).json({
+      msg: 'success',
+      user: req.user
+    });
+  }
+);
+
+//login with linkedin
+router.get('/auth/linkedin', passport.authenticate('linkedin'));
+router.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+module.exports = router;
